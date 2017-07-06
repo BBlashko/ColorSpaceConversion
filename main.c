e79cea2b3e9f89e5dd516be9c992024a0e64f5ea
Brett Blashko <blashko.brett@gmail.com>
2017-07-06 14:29:35 -0700

    init

---
+++ main.c
@@ -0,0 +1,15 @@
+/* Color Space Conversion
+ * Authors: Brett Blashko
+ *          Brandon Harvey
+ * Date Created: July 6, 2017
+ * Modified Date: July6, 2017
+ * Purpose: Optimize Color Space conversions
+ */
+
+#include <stdio.h>
+
+int main()
+{
+    printf("%s\n", "Hello Color Space Conversion");
+    return 0;
+}
